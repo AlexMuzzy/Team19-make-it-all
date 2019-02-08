4925b4040d0be4d52e1327e26282a6b35615497b
AlexMuzzy <alexmuzzy33@gmail.com>
2019-02-08 13:50:04 +0000

    commit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['name' => 'Team 19 Web Application']);
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
